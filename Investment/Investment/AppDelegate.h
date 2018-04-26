f9d9b5f898e1a66d080db69b849ecd8e93440c09
王雷 <wanglei@Hong.local>
2018-04-26 11:03:29 +0800

    the one

---
+++ Investment/Investment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Investment
+//
+//  Created by 王雷 on 2018/4/26.
+//  Copyright © 2018年 王雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
